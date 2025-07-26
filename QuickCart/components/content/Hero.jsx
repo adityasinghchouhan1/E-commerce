93ae1bc7f280b28dff3efa067a5bd3700ac40fd7
adityasinghchouhan1 <asaditysingh2572000@gmail.com>
2025-07-26 11:40:39 +0530

    fixed error

--- QuickCart/components/content/Hero.jsx
+++ QuickCart/components/content/Hero.jsx
@@ -1,32 +1,51 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper/modules'
+import axios from 'axios'
 import Image from 'next/image'
 import 'swiper/css'
-
-const images = ['/banner1.webp', '/unlockbanner.webp', '/banner1.webp']
+import 'swiper/css/autoplay'
 
 export default function Hero() {
+  const [sliderImages, setSliderImages] = useState([])
+
+  useEffect(() => {
+    const fetchSliderImages = async () => {
+      try {
+        const res = await axios.get('http://localhost:8008/api/getsliderdata')
+        if (res.data.status && res.data.data) {
+          const imagePaths = res.data.data.map((item) => item.image)
+          setSliderImages(imagePaths)
+        }
+      } catch (error) {
+        console.error('Error fetching slider images:', error)
+      }
+    }
+
+    fetchSliderImages()
+  }, [])
+
   return (
-    <div className="relative w-full  h-[70vh] sm:h-[70vh] xs:h-[40vh]">
+    <div className="relative w-full h-[70vh] sm:h-[70vh] xs:h-[40vh]">
       <Swiper
         modules={[Autoplay]}
         autoplay={{
           delay: 3000,
           disableOnInteraction: false,
         }}
-        speed={1000}
         loop={true}
+        speed={1000}
         slidesPerView={1}
         spaceBetween={0}
         className="w-full h-full"
       >
-        {images.map((src, index) => (
+        {sliderImages.map((filename, index) => (
           <SwiperSlide key={index}>
-            <div className="relative mt-1 w-full h-[70vh] sm:h-[70vh] xs:h-[40vh]">
+            <div className="relative w-full h-[70vh] sm:h-[70vh] xs:h-[40vh]">
               <Image
-                src={src}
+                src={`http://localhost:8008/uploads/${filename}`}
                 alt={`Slide ${index + 1}`}
                 fill
                 priority
